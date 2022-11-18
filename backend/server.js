1aa5f7b1ded23d81f7512920330f649bd48ae936
Hitesh Tarwani <hiteshtarwani4@gmail.com>
2022-11-18 18:59:57 +0530

    check3

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routes/goalRoutes"));
-app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/users", require("./routes/userroutes"));
 
 //server Frontend
 if (process.env.NODE_ENV === "production") {
